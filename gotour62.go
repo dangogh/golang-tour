e1f96138cea28e0063f06d9da6a3cea9039870ef
Dan Kirkwood <dangogh@gmail.com>
2014-03-16 16:29:12 -0600

    exercises 62, 63.  Cleanup

---
+++ gotour62.go
@@ -0,0 +1,29 @@
+package main
+import (
+    "code.google.com/p/go-tour/pic"
+    "image"
+    "image/color"
+)
+
+type Image struct{
+    c color.Model
+    w, h int
+}
+
+func (img Image) ColorModel() color.Model {
+    return img.c
+}
+
+func (img Image) Bounds() image.Rectangle {
+    return image.Rect(0, 0, img.w, img.h)
+}
+
+func (img Image) At(x, y int) color.Color {
+    return color.RGBA{0, 0, 255, 255}
+}
+
+func main() {
+    m := Image{color.RGBAModel, 10, 20}
+    pic.ShowImage(m)
+}
+
