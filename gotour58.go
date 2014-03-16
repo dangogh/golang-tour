e6a320f68d2a8846a4b5ce2f432f1af016c1ece6
Dan Kirkwood <dangogh@gmail.com>
2014-03-15 19:36:59 -0600

    Create gotour58.go

---
+++ gotour58.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+    "math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprint("cannot Sqrt negative number: ",
+		float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+    if x < 0 {
+    	return 0, ErrNegativeSqrt(x)
+    }
+	oldz, z := x, x/2.0
+	ii := 0
+	for math.Abs(z-oldz) > 1e-15 {
+		oldz, z = z, z-(math.Pow(z, 2)-x)/(2*z)
+		ii++
+	}
+	fmt.Println("Took ", ii, " iterations")
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
