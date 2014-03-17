6865b1b5baa4653a291dcecb4abd9f6317f1db26
Dan Kirkwood <dangogh@gmail.com>
2014-03-16 21:22:16 -0600

    cracked the code

--- gotour63.go
+++ gotour63.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-    "io"
-    "os"
-    "strings"
-)
+		"io"
+		"os"
+		"strings"
+       )
 
 type rot13Reader struct {
-    r io.Reader
+	r io.Reader
+}
+
+func rot13Char(ch byte) byte {
+	switch {
+	case ch >= 'a' && ch <= 'm':
+		ch += 13
+	case ch >= 'A' && ch <= 'M':
+		ch += 13
+	case ch >= 'n' && ch <= 'z':
+		ch -= 13
+	case ch >= 'N' && ch <= 'Z':
+		ch -= 13
+	}
+	return ch
+}
+
+func (rot13 rot13Reader) Read(p []byte) (int, error) {
+	n, err := rot13.r.Read(p)
+	for ii, ch := range p {
+		p[ii] = rot13Char(ch)
+	}
+	return n, err
 }
 
 func main() {
-    s := strings.NewReader(
-        "Lbh penpxrq gur pbqr!")
-    r := rot13Reader{s}
-    io.Copy(os.Stdout, &r)
+	s := strings.NewReader(
+		   "Lbh penpxrq gur pbqr!")
+	   r := rot13Reader{s}
+   io.Copy(os.Stdout, &r)
 }
